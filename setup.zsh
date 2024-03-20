c168c682a4558341dcc4f1686c0632c5f3116dd4
Daniel Kulas <danielkulas@proton.me>
2024-03-19 22:11:52 -0700

    setup script complete, added build actions to generate a macOS app bundle to allow for running the script through an app instead of through the terminal for ease of use

--- setup.zsh
+++ setup.zsh
@@ -7,24 +7,14 @@
 #   Someone on my youtube video commented that they have an Intel Macbook and cannot use the pre-built Apple Silicon macOS binary
 #   I suggested to follow the readme on how to setup up their system to run from he source code but they were not well-versed 
 #   in terminal commands. This is an attempt at streamlining that process.
+#   Platypus is used to to package up the script into an .app bundle
 
 apexsense_version="1.0.1"
 
-echo "🏎️  ApexSense ${apexsense_version} development environment quick setup.\n"
+apexsense_output=$HOME/ApexSense-${apexsense_version}
 
-xcode-select -p &>/dev/null
-if [ $? -ne 0 ]; then
-    echo ""
-    echo "⚠️ Xcode command line tools are not installed ⚠️"
-    echo ""
-    echo "A window will appear asking to install these tools."
-    echo "These tools are required to run ApexSense from the source code."
-    echo "Click on Install, accept Apple's license agreement, and run ApexSense Setup again after installation completes."
-    xcode-select --install
-    echo "You can click on the 'Quit' button to dismiss this message." 
-else 
-    apexsense_output=$HOME/ApexSense-${apexsense_version}
-
+###### Functions ######
+launch_apexsense_if_ready() {
     if [ -f "${apexsense_output}/package.json" ]; then
         echo "Launching ApexSense from the source code..."
         cd ${apexsense_output}
@@ -32,53 +22,114 @@ else
         npm start
         exit 0
     fi
+}
 
-    echo "ApexSense source files will be saved in ${apexsense_output}\n\n"
+check_for_xcode_cli_tools() {
+    xcode-select -p &>/dev/null
+    if [ $? -ne 0 ]; then
+        echo ""
+        echo "⚠️ Xcode command line tools are not installed ⚠️"
+        echo ""
+        $(xcode-select --install) &
+        echo ""
+        echo "A window will appear asking to install these tools."
+        echo "These tools are required to run ApexSense from the source code."
+        echo ""
+        echo "Click on Install, accept Apple's license agreement, then wait for the software to download and install."
+        echo ""
+        echo "Keep this window open while the Xcode command line tools are downloading..."
+        echo ""
+        echo "When the download completes, click on 'Done' in the Xcode command line tools installer window to continue." 
 
-    # Python setup
-    # `xcode-select --install` will install a valid instance of python
-    # Invoking python3 before installing the xcode cli tools will trigger 
-    # a UI prompt to inform the end-user to install them
-    py_version=$(python3 --version)
-    if [[ $py_version == *"No developer tools"* ]]; then
-        echo "Ensure you have Xcode Command Line Tools installed"
-        xcode-select --install
-        exit -1
-    else
-        echo "Found $(python3 --version)."
-        numpy_installed=false
-        opencv_installed=false
+        sleep 5
 
-        if python3 -c "import numpy" 2>/dev/null; then numpy_installed=true; fi
-        if python3 -c "import cv2" 2>/dev/null; then opencv_installed=true; fi
-
-        if ! $numpy_installed || ! $opencv_installed; then
-            echo "Installing numpy, opencv-python..."
-            pip3 install numpy opencv-python --quiet &>/dev/null
-        fi
+        local installer_proc_name="Install Command Line Developer Tool"
+        while pgrep -f "${installer_proc_name}" > /dev/null; do
+            sleep 3
+        done
     fi
 
-    # Node.js install
+    if ! [ -d "/Library/Developer/CommandLineTools" ]; then
+        echo "The Xcode command line developer tools did not appear to install correctly."
+        echo "Exiting..."
+        exit -1
+    fi
+}
+
+setup_python() {
+    # `xcode-select --install` will install a valid instance of python
+    py_version=$(python3 --version)
+    if [[ $py_version == *"No developer tools"* ]]; then
+        echo "Ensure you have Xcode Command Line Tools installed."
+        exit -1
+    fi
+
+    echo "Found $(python3 --version)."
+
+    numpy_installed=false
+    opencv_installed=false
+    if python3 -c "import numpy" 2>/dev/null; then numpy_installed=true; fi
+    if python3 -c "import cv2" 2>/dev/null; then opencv_installed=true; fi
+
+    if ! $numpy_installed || ! $opencv_installed; then
+        echo "Installing numpy, opencv-python..."
+        pip3 install numpy opencv-python --quiet &>/dev/null
+    fi
+}
+
+install_nvm_node() { 
     if ! command -v npm; then
-        echo "Installing Node.js"
+        echo "Installing Node.js..."
         touch ~/.zshrc
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh &>/dev/null | bash &>/dev/null
         source ~/.zshrc
         nvm install node &>/dev/null
     fi
+}
 
-    # ApexSense source code setup
+download_apexsense() {
     apexsense_zip_output=/tmp/ApexSense-${apexsense_version}.zip
-    echo "Downloading ApexSense v${apexsense_version} source code to ${apexsense_zip_output}"
-    curl -L "https://github.com/shaboinkin90/ApexSense/archive/refs/tags/v${apexsense_version}.zip" -o ${apexsense_zip_output} &>/dev/null
-    unzip -q ${apexsense_zip_output} -d $HOME
-    rm ${apexsense_zip_output}
+    src_url="https://github.com/shaboinkin90/ApexSense/archive/refs/tags/v${apexsense_version}.zip" 
+    echo "Downloading ApexSense v${apexsense_version} source code from ${src_url}"
+    curl -L $src_url -o $apexsense_zip_output &>/dev/null
+    unzip -q $apexsense_zip_output -d $HOME
+    rm $apexsense_zip_output
+}
 
+prepare_and_launch_apexsense() {
     echo "Downloading dependencies for ApexSense..."
     cd ${apexsense_output}
     npm install &>/dev/null
 
+    if ! [ -d $apexsense_output/node_modules ]; then
+        # On testing in a virtual machine, sometimes the download would fail. Running again "works"
+        npm install &>/dev/null
+    fi
     npm start
+}
 
-    echo "Setup Complete ✅"
-fi
+###### Start #######
+
+launch_apexsense_if_ready # will exit
+
+echo "\n🏎️  ApexSense ${apexsense_version} development environment quick setup.\n"
+
+check_for_xcode_cli_tools # can exit
+
+echo "ApexSense source files will be saved in ${apexsense_output}"
+
+echo "1️⃣"
+setup_python # can exit
+
+echo "2️⃣"
+install_nvm_node
+
+echo "3️⃣"
+download_apexsense
+
+echo "4️⃣"
+prepare_and_launch_apexsense
+
+echo "Setup Complete ✅"
+echo ""
+echo "Run ApexSense Dev Mode.app again to launch ApexSense."
