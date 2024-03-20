8606aae5866b127eabe0069f32f9d5337afa195d
Daniel Kulas <danielkulas@proton.me>
2024-03-20 15:19:04 -0700

    Cleaned up the script and fixed various syntax related things

--- setup.zsh
+++ setup.zsh
@@ -9,15 +9,45 @@
 #   in terminal commands. This is an attempt at streamlining that process.
 #   Platypus is used to to package up the script into an .app bundle
 
-apexsense_version="1.0.1"
 
-apexsense_output=$HOME/ApexSense-${apexsense_version}
+
+
+# Extract the tag_name from the JSON response, which is the version number
+
+apexsense_version=""
+apexsense_output=""
 
 ###### Functions ######
+get_latest_apexsense_version() {
+    github_releases_url="https://api.github.com/repos/shaboinkin90/ApexSense/releases/latest"
+    latest_release_json=$(curl -s "$github_releases_url")
+    local curl_exit_status=$?
+    if [[ $curl_exit_status -ne 0 ]]; then
+        echo "‼️ Failed to query ${github_releases_url}. ‼️" >&2
+        echo "Check your network connection or check if you can access the ApexSense repository on Github at https://github.com/shaboinkin90/ApexSense/releases/" >&2
+        echo "Exiting..." >&2
+        exit 1
+    fi
+
+    latest_version=$(echo "${latest_release_json}" | grep '"tag_name":' | sed -E 's/.*"tag_name": "([^"]+)".*/\1/')
+    echo "$latest_version"
+}
+
 launch_apexsense_if_ready() {
-    if [ -f "${apexsense_output}/package.json" ]; then
+    if [[ -z "$apexsense_output" ]]; then
+        # problem getting the version number, maybe due to network issue querying github, see if a copy exists
+        apexsense_output=$(find "${HOME}" -type d -name 'ApexSense-v*' | sort -V | tail -n 1)
+        if [[ -z "$apexsense_output" ]]; then
+            echo "Could not determine the latest version of ApexSense to download nor did it find an existing copy of ApexSense."
+            echo "Check your network connection or check if you can access the ApexSense repository on Github at https://github.com/shaboinkin90/ApexSense/releases/"
+            echo "Exiting..."
+            exit 1
+        fi
+    fi
+
+    if [[ -f "${apexsense_output}/package.json" ]]; then
         echo "Launching ApexSense from the source code..."
-        cd ${apexsense_output}
+        cd "$apexsense_output"
         source ~/.zshrc
         npm start
         exit 0
@@ -26,22 +56,20 @@ launch_apexsense_if_ready() {
 
 check_for_xcode_cli_tools() {
     xcode-select -p &>/dev/null
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 ]]; then
         echo ""
         echo "⚠️ Xcode command line tools are not installed ⚠️"
         echo ""
-        $(xcode-select --install) &
+        xcode-select --install
         echo ""
         echo "A window will appear asking to install these tools."
         echo "These tools are required to run ApexSense from the source code."
         echo ""
         echo "Click on Install, accept Apple's license agreement, then wait for the software to download and install."
         echo ""
-        echo "Keep this window open while the Xcode command line tools are downloading..."
+        echo "Keep this window open while the Xcode command line tools finish installing..."
         echo ""
-        echo "When the download completes, click on 'Done' in the Xcode command line tools installer window to continue." 
-
-        sleep 5
+        echo "When the installation completes, click on 'Done' in the Xcode command line tools installer window to continue." 
 
         local installer_proc_name="Install Command Line Developer Tool"
         while pgrep -f "${installer_proc_name}" > /dev/null; do
@@ -49,29 +77,29 @@ check_for_xcode_cli_tools() {
         done
     fi
 
-    if ! [ -d "/Library/Developer/CommandLineTools" ]; then
-        echo "The Xcode command line developer tools did not appear to install correctly."
-        echo "Exiting..."
-        exit -1
+    if [[ !  -d "/Library/Developer/CommandLineTools" ]]; then
+        echo "The Xcode command line developer tools did not appear to install correctly." >&2
+        echo "Exiting..." >&2
+        exit 1
     fi
 }
 
 setup_python() {
     # `xcode-select --install` will install a valid instance of python
-    py_version=$(python3 --version)
+    local py_version=$(python3 --version)
     if [[ $py_version == *"No developer tools"* ]]; then
         echo "Ensure you have Xcode Command Line Tools installed."
-        exit -1
+        exit 1
     fi
 
-    echo "Found $(python3 --version)."
+    echo "Found $py_version."
 
-    numpy_installed=false
-    opencv_installed=false
-    if python3 -c "import numpy" 2>/dev/null; then numpy_installed=true; fi
-    if python3 -c "import cv2" 2>/dev/null; then opencv_installed=true; fi
+    local numpy_installed="false"
+    local opencv_installed="false"
+    if python3 -c "import numpy" 2>/dev/null; then numpy_installed="true"; fi
+    if python3 -c "import cv2" 2>/dev/null; then opencv_installed="true"; fi
 
-    if ! $numpy_installed || ! $opencv_installed; then
+    if [[ $numpy_installed != "true" ]] || [[ $opencv_installed != "true" ]]; then
         echo "Installing numpy, opencv-python..."
         pip3 install numpy opencv-python --quiet &>/dev/null
     fi
@@ -81,55 +109,89 @@ install_nvm_node() {
     if ! command -v npm; then
         echo "Installing Node.js..."
         touch ~/.zshrc
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh &>/dev/null | bash &>/dev/null
+        curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh" &>/dev/null | bash &>/dev/null
         source ~/.zshrc
         nvm install node &>/dev/null
     fi
 }
 
 download_apexsense() {
-    apexsense_zip_output=/tmp/ApexSense-${apexsense_version}.zip
-    src_url="https://github.com/shaboinkin90/ApexSense/archive/refs/tags/v${apexsense_version}.zip" 
-    echo "Downloading ApexSense v${apexsense_version} source code from ${src_url}"
-    curl -L $src_url -o $apexsense_zip_output &>/dev/null
-    unzip -q $apexsense_zip_output -d $HOME
-    rm $apexsense_zip_output
+    local temp_zip_output=$(mktemp -d)
+    if [[ ! -d "$temp_zip_output" ]]; then 
+        echo "Failed to create a temporary directory to store the zip." >&2
+        echo "Exiting..."
+        exit 1
+    fi
+    
+    local apexsense_zip_output="${temp_zip_output}/ApexSense-${apexsense_version}.zip"
+    local src_url="https://github.com/shaboinkin90/ApexSense/archive/refs/tags/${apexsense_version}.zip" 
+    
+    echo "Downloading ApexSense ${apexsense_version} source code from ${src_url}"
+    curl -L "$src_url" -o "$apexsense_zip_output" &>/dev/null
+    local curl_exit_status=$?
+    if [[ $curl_exit_status -ne 0 ]]; then
+        echo "‼️ Failed to download from ${src_url}. ‼️"
+        echo "Check your network connection or check if you can access the ApexSense repository on Github at ${src_url}" >&2
+        echo "Exiting..." >&2
+        exit 1
+    fi
+
+    local temp_zip_extract=$(mktemp -d)
+    if [[ ! -d "$temp_zip_extract" ]]; then 
+        echo "Failed to create a temporary directory to extract the zip." >&2
+        echo "Exiting..."
+        exit 1
+    fi
+
+    unzip -q "$apexsense_zip_output" -d "$temp_zip_extract"
+    mv "${temp_zip_extract}"/* "$apexsense_output"
+    trap "rm -rf ${temp_zip_output} ${temp_zip_extract}" EXIT
 }
 
 prepare_and_launch_apexsense() {
-    echo "Downloading dependencies for ApexSense..."
-    cd ${apexsense_output}
+    echo "Downloading dependencies for ApexSense...this might take a few minutes..."
+    cd $apexsense_output
     npm install &>/dev/null
 
-    if ! [ -d $apexsense_output/node_modules ]; then
+    if [[ ! -d "${apexsense_output}/node_modules" ]]; then
         # On testing in a virtual machine, sometimes the download would fail. Running again "works"
         npm install &>/dev/null
     fi
-    npm start
 }
 
 ###### Start #######
 
-launch_apexsense_if_ready # will exit
+
+apexsense_version=$(get_latest_apexsense_version)
+if [[ $apexsense_version =~ v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    apexsense_output=$HOME/ApexSense-${apexsense_version}
+fi
+
+launch_apexsense_if_ready
+
 
 echo "\n🏎️  ApexSense ${apexsense_version} development environment quick setup.\n"
 
 check_for_xcode_cli_tools # can exit
 
-echo "ApexSense source files will be saved in ${apexsense_output}"
+echo "\n🟣 ApexSense source files will be saved in ${apexsense_output} 🟣"
 
-echo "1️⃣"
+echo "🔵⚪️⚪️⚪️" 
 setup_python # can exit
 
-echo "2️⃣"
+echo "🟢🔵⚪️⚪️"
 install_nvm_node
 
-echo "3️⃣"
+echo "🟢🟢🔵⚪️"
 download_apexsense
 
-echo "4️⃣"
+echo "🟢🟢🟢🔵"
 prepare_and_launch_apexsense
 
-echo "Setup Complete ✅"
+echo "🟢🟢🟢🟢"
+echo "Setup Complete"
+
+npm start
+
 echo ""
 echo "Run ApexSense Dev Mode.app again to launch ApexSense."
